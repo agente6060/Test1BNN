57bf8d8975a740cf7e8b2b9eed2bc5e3d3d77c35
Manuel Martinez Marcos <isc.ma3.unix@hotmail.com>
2016-12-01 11:58:43 -0600

    Valida si una lista de palabras desde archivo son palindromos

---
+++ Business/BPalindromo.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class BPalindromo
+    {
+        /// <summary>
+        /// Valida si la palabra es palindromo o no
+        /// </summary>
+        /// <param name="palabra">Cadena a validar si es palindromo</param>
+        /// <returns>Indica si es palindromo la palabra</returns>
+        internal bool EsPalindromo(string palabra)
+        {
+            string palabraTemp = string.Empty;
+            int lonPalabra = palabra.Length;
+
+            try
+            {
+                for (int i = lonPalabra - 1; i >= 0; i--)
+                {
+                    palabraTemp = palabraTemp + palabra.Substring(i, 1);
+                }
+                return palabra == palabraTemp;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Palindromo> ValidarPalindromos(List<String> palabras)
+        {
+            List<Palindromo> result = new List<Palindromo>();
+
+            foreach (string palabra in palabras)
+            {
+                if (!string.IsNullOrWhiteSpace(palabra))
+                { 
+                    result.Add(new Palindromo { Palabra= palabra, EsPalindromo = EsPalindromo(palabra) });
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsolePalindromo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Business;
+using Entities;
+
+namespace ConsolePalindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> palabras = new List<string>();
+            string palabra = string.Empty;
+
+            FileInfo file = new FileInfo(@"c:\test.txt");
+
+            if (!file.Exists)
+            {
+                Console.WriteLine(@"Por favor agregue el archivo test.txt en 'c:\' y ejecute de nuevo la aplicación");
+                Console.ReadKey();
+                return;
+            }
+
+            StreamReader archivo = new StreamReader(file.FullName);
+
+            while ((palabra=archivo.ReadLine()) != null)
+            {
+                palabras.Add(palabra);
+            }
+
+            BPalindromo buss = new BPalindromo();
+
+            var palindromos = buss.ValidarPalindromos(palabras);
+
+            foreach (Palindromo palindromo in palindromos)
+            {
+                Console.WriteLine($"{palindromo.Palabra}: {palindromo.EsPalindromo}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
